2b065572b0b0b4ca4417c55e1ab3688d4eae631f
NamNam201 <19T1051037@husc.edu.vn>
2024-08-28 21:35:22 -0700

    deploy_BE

--- SOFTWARE/webApp/FE/src/CustomLayout.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { Layout } from 'react-admin';
-import CustomMenu from './CustomMenu'; // Menu tùy chỉnh bạn đã tạo
-
-const CustomLayout = (props) => <Layout {...props} menu={CustomMenu} />;
-
-export default CustomLayout;
