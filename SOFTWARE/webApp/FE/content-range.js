1d03ca5d7d42322b0bd8ccbd97e5757afd22d66d
NamNam201 <19T1051037@husc.edu.vn>
2024-08-29 00:56:32 -0700

    Fe Demo

--- SOFTWARE/webApp/FE/content-range.js
+++
@@ -1,5 +0,0 @@
-module.exports = (req, res, next) => {
-    res.header('Access-Control-Expose-Headers', 'Content-Range');
-    res.header('Content-Range', `devices 0-10/20 ; dashboards 0-2/4;`);
-    next();
-}
